a8acd62ed8650845d55dafd56adb1ae68ac003b8
dannylongeuay <dannylongeuay@yahoo.com>
2022-09-28 11:33:00 -0700

    chore(github/workflows): :wrench: update release and publish

--- app/main.py
+++ app/main.py
@@ -40,7 +40,7 @@ tags_metadata = [
 app = FastAPI(
     title="Pyapi",
     description="Python API using FastAPI library",
-    version="1.0.0",
+    version="1.0.1",
     openapi_tags=tags_metadata,
 )
 app.add_middleware(
